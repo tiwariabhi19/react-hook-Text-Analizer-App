647b4f2a55ad347de9275151e71fedb4b5907bb9
tiwariabhi19 <abhishektiwari641999@gmail.com>
2024-05-05 08:42:56 +0530

    Text Analizer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import './App.css'
+// import About from './components/About';
 
 import Navbar  from './components/Navbar';
 import TextForm from './components/TextForm';
@@ -7,11 +9,23 @@ import TextForm from './components/TextForm';
 
 
 function App() {
+  const [mode, setMode] = useState('light')
+
+  const toggleMode = () => {
+    if (mode === "light"){
+      setMode('dark')
+      document.body.style.backgroundColor = "#042743";
+    }else{
+      setMode('light')
+      document.body.style.backgroundColor = "white";
+    }
+  }
   return (
     <>
-      <Navbar/>
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
       <div className="container my-3" >
-        <TextForm heading="Enter the text to analize below"/>
+        { <TextForm heading="Enter the text to analize below" mode={mode} /> }
+        {/* <About/> */}
       </div>
     </>
   )
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -19,6 +19,16 @@ export default function TextForm(props) {
         setText(newText)
     }
 
+    const handledCopy = () => {
+        var text = document.getElementById("textBox")
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
+
+    const handledExtraSpace = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "))
+    }
 
     const handledOnChange = (event) => {
         console.log("convert to upper case")
@@ -27,21 +37,23 @@ export default function TextForm(props) {
     
     return (
         <>
-        <div>
+        <div className="container" style={{color: props.mode === 'dark'? 'white': '#042743'}}>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-            <textarea className="form-control" id="textBox" onChange={handledOnChange} value={text} rows="8"></textarea>
+            <textarea className="form-control" id="textBox" style={{backgroundColor: props.mode === 'dark'? 'gray': 'white', color: props.mode === 'dark'? 'white': '#042743'}} onChange={handledOnChange} value={text} rows="8"></textarea>
             </div>
             <button className='btn btn-primary mx-1' onClick={handledUpClick}>Convert to Upper</button>
             <button className='btn btn-primary mx-1' onClick={handledLoClick}>Convert to Lower</button>
             <button className='btn btn-primary mx-1' onClick={handledCleanClick}>Clear Content</button>
+            <button className='btn btn-primary mx-1' onClick={handledCopy}>Copy Content</button>
+            <button className='btn btn-primary mx-1' onClick={handledExtraSpace}>Removed Extra Space</button>
         </div>
-        <div className="container my-2">
+        <div className="container my-2" style={{color: props.mode === 'dark'? 'white': '#042743'}}>
             <h2>Your text summary</h2>
             <p>{text.split(" ").length} Words and Charactors {text.length} </p>
             <p>{0.008*text.split(" ").length } Minutes read  </p>
             <h3>Preview</h3>
-            <p>{text}</p>
+            <p>{text.length>0? text: "Enter something in text box to preview here.."}</p>
         </div>
         </>
     )
